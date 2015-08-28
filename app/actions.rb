5087c9608a87ab872a297719b27f4202dbc82699
Will Beange <will@convertstar.com>
2015-08-28 12:12:37 -0700

    basic instagram setup

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,23 @@
 get '/' do
   erb :index
 end
+
+# Instagram Auth
+get "/oauth/connect" do
+  redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+end
+
+get "/oauth/callback" do
+  response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+  session[:access_token] = response.access_token
+  redirect "/media_popular"
+end
+
+get "/media_popular" do
+  client = Instagram.client(:access_token => session[:access_token])
+  html = "<h1>Get a list of the overall most popular media items</h1>"
+  for media_item in client.media_popular
+    html << "<img src='#{media_item.images.thumbnail.url}'>"
+  end
+  html
+end
--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,8 @@ require 'sinatra/contrib/all' # Requires cookies, among other things
 
 require 'pry'
 
+require 'instagram'
+
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 APP_NAME = APP_ROOT.basename.to_s
 
@@ -24,8 +26,22 @@ configure do
   set :views, File.join(Sinatra::Application.root, "app", "views")
 end
 
+enable :sessions
+
+# instagram-ruby-gem configuration
+CALLBACK_URL = "http://localhost:3001/oauth/callback"
+CLIENT_ID = "d119273d693948dea278436882eb2536"
+CLIENT_SECRET = "e20f05e3f1eb45a9bd82d1db9ddf97e8"
+
+Instagram.configure do |config|
+  config.client_id = CLIENT_ID
+  config.client_secret = CLIENT_SECRET
+end
+
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
 
 # Load the routes / actions
 require APP_ROOT.join('app', 'actions')
+
+
