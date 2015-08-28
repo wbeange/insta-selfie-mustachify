2adb1e999808893b52b967bfee22346599868995
Will Beange <will@convertstar.com>
2015-08-28 14:03:51 -0700

    implemented latest selfie view

--- app/actions.rb
+++ app/actions.rb
@@ -11,14 +11,17 @@ end
 get "/oauth/callback" do
   response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
   session[:access_token] = response.access_token
-  redirect "/media_popular"
+  redirect "/selfie"
 end
 
-get "/media_popular" do
+get "/selfie" do
   client = Instagram.client(:access_token => session[:access_token])
-  html = "<h1>Get a list of the overall most popular media items</h1>"
-  for media_item in client.media_popular
-    html << "<img src='#{media_item.images.thumbnail.url}'>"
-  end
-  html
+
+  tags = client.tag_search('selfie')
+
+  media = client.tag_recent_media(tags.first.name)
+
+  @media_item = media.first
+
+  erb :selfie
 end
