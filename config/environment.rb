593b293857bae7c107e0a7ffc2ba7664d69ef6ae
Will Beange <will@convertstar.com>
2015-08-29 11:43:18 -0700

    no idea

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require 'active_support/all'
 
 # Load Sinatra Framework (with AR)
 require 'sinatra'
-require 'sinatra/activerecord'
+# require 'sinatra/activerecord'
 require 'sinatra/contrib/all' # Requires cookies, among other things
 
 require 'pry'
